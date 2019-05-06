a7cbdf10d544ee22801e60e0ae71d5f98b189b09
JonathanLangton1 <jonathanlangton3@gmail.com>
2019-05-06 14:48:11 +0100

    responsive design plus image slide improvements

--- resources/javascript.js
+++ resources/javascript.js
@@ -5,6 +5,23 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
+    $("button.menu-toggle").click(function(){
+        $(".main-menu-container").toggleClass("left-0");
+    });
+});
+
+$(document).ready(function(){
+
+var button = document.getElementById( 'menu-toggle' );
+
+    $(".main-menu-item").click(function(){
+        $(".main-menu-container").toggleClass("left-0");
+        button.className = button.className.replace( ' opened', '' );
+        button.setAttribute( 'aria-expanded', 'false' );
+    });
+});
+
+$(document).ready(function(){
     $(".fade-in").ready(function(){
         $(".fade-in").toggleClass("visible");
     });
@@ -28,15 +45,32 @@ $(".arrow-click").click(function() {
   var box = $(".image-container"),
     x;
   if ($(this).hasClass("arrow-right")) {
-    x = ((box.width() / 2)) + box.scrollLeft();
+    x = ((box.width() / 1)) + box.scrollLeft();
     box.animate({
       scrollLeft: x,
     })
   } else {
-    x = ((box.width() / 2)) - box.scrollLeft();
+    x = ((box.width() / 1)) - box.scrollLeft();
     box.animate({
       scrollLeft: -x,
     })
   }
 })
+});
+
+
+$(document).ready(function(){
+var button = document.getElementById( 'menu-toggle' );
+
+button.onclick = function() {
+
+  if ( -1 !== button.className.indexOf( 'opened' ) ) {
+    button.className = button.className.replace( ' opened', '' );
+    button.setAttribute( 'aria-expanded', 'false' );
+  } else {
+     button.className += ' opened';
+    button.setAttribute( 'aria-expanded', 'true' );
+   }
+
+ };
 });
